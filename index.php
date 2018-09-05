918bdd3fd6dd161acfd78d6fcf25efb27e409311
yamanmnur <yamanmnur0401@gmail.com>
2018-09-05 08:52:59 +0700

    pertama

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/jquery.mobile.flatui.css" />
+  <script src="js/jquery.js"></script>
+  <script src="js/jquery.mobile-1.4.0-rc.1.js"></script>
+</head>
+<body>
+  	<div data-role="page">
+
+    
+
+		<div data-role="header" data-position="fixed">
+			<a data-iconpos="notext" data-role="button" data-icon="home" title="Home">Home</a>
+			<h1>A</h1>
+			<a  href="#panel" data-role="button" data-icon="search">search</a>
+		</div>
+
+		<div data-role="content" role="main">
+		<p>Sample text and <a href="#">links</a>.</p>
+		<p>Sample text and <a href="#">links</a>.</p>
+		<p>Sample text and <a href="#">links</a>.</p>
+		<p>Sample text and <a href="#">links</a>.</p>
+		<p>Sample text and <a href="#">links</a>.</p>
+		<p>Sample text and <a href="#">links</a>.</p>
+		<p>Sample text and <a href="#">links</a>.</p>
+		<p>Sample text and <a href="#">links</a>.</p>
+		<p>Sample text and <a href="#">links</a>.</p>
+		<p>Sample text and <a href="#">links</a>.</p>
+		<p>Sample text and <a href="#">links</a>.</p>
+		<p>Sample text and <a href="#">links</a>.</p>
+		<p>Sample text and <a href="#">links</a>.</p>
+		<p>Sample text and <a href="#">links</a>.</p>
+			<div data-role="collapsible-set" data-theme="b" data-content-theme="b">
+				<div data-role="collapsible" data-collapsed-icon="flat-time" data-expanded-icon="flat-cross" data-collapsed="false">
+				  <h3>Section 1</h3>
+				  <p>I'm the collapsible content for section 1</p>
+				</div>
+				<div data-role="collapsible" data-collapsed-icon="flat-calendar" data-expanded-icon="flat-cross">
+				  <h3>Section 2</h3>
+				  <p>I'm the collapsible content for section 2</p>
+				</div>
+				<div data-role="collapsible" data-collapsed-icon="flat-settings" data-expanded-icon="flat-cross">
+				  <h3>Section 3</h3>
+				  <p>I'm the collapsible content for section 3</p>
+				</div>
+			  </div>
+	
+
+		
+	
+		</div>
+		<div data-role="footer" data-position="fixed">
+			<center>
+					<p >yamanmnur@gmail.com</p>
+					<a href="#" data-role="button" data-icon="plus">add facebook</a>
+			</center>	
+		</div>
+	</div>
+  
+</body>
+</html>
